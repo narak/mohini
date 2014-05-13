43287ccaf5d8b08909e005f91353fdd4b40240e4
= <=>
2014-05-13 21:51:22 +0530

    Connector cleanup #1

--- js/connector.js
+++ js/connector.js
@@ -49,6 +49,28 @@ function updateAt(thisProp, diagProp, opts) {
     }
 }
 
+function addConnector() {
+    var uuid = generateUUID('conn'),
+        connector;
+
+    connectorMap[uuid] = connector = {
+        _diagonal: d3.svg.diagonal(),
+        startsAt: startsAt,
+        endsAt: endsAt
+    };
+
+    // Start connector.
+    connector.el = connectorContainer.append('path')
+        .attr('class', 'connector')
+        .attr('uuid', uuid);
+
+    d3.select('body').on('mousemove.connectorPoint', function() {
+        connector.endsAt({ coords: translateNScale(d3.event) })
+    });
+
+    return connector;
+}
+
 function connectorPoint(group) {
     group.on('mousedown.connectorPoint', function() {
             if (!isPoint(d3.event.target)) return;
@@ -58,29 +80,11 @@ function connectorPoint(group) {
         .on('click.connectorPoint', function(d) {
             if (!isPoint(d3.event.target)) return;
 
-            var tns = translateNScale(d3.event),
-                compUUID = d3.select(this).attr('uuid'),
-                component = getComponent(compUUID),
-                ctns = component.getEndPointCoords(),
-                uuid;
+            var compUUID = d3.select(this).attr('uuid'),
+                component = getComponent(compUUID);
 
             if (!connector) {
-                uuid = generateUUID('conn');
-                connectorMap[uuid] = connector = {
-                    _diagonal: d3.svg.diagonal(),
-                    startsAt: startsAt,
-                    endsAt: endsAt
-                };
-
-                // Start connector.
-                connector.el = connectorContainer.append('path')
-                    .attr('class', 'connector')
-                    .attr('uuid', uuid);
-
-                d3.select('body').on('mousemove.connectorPoint', function() {
-                    connector.endsAt({ coords: translateNScale(d3.event) })
-                });
-
+                connector = addConnector();
                 connector.startsAt({ component: component, render: false });
                 component.connectors.startsAt.push(connector);
             } else {
